8f359c6338cf1e5ce7e96237583947112ae7c45b
Ian Dunn <iondune@gmail.com>
2017-11-06 23:06:01 -0800

    Consistent spacing and indentation, remove default constructors and destructors

--- src/Shape.cpp
+++ src/Shape.cpp
@@ -1,3 +1,4 @@
+
 #include "Shape.h"
 #include <iostream>
 #include <assert.h>
@@ -7,62 +8,50 @@
 
 using namespace std;
 
-Shape::Shape() :
-	eleBufID(0),
-	posBufID(0),
-	norBufID(0),
-	texBufID(0),
-   vaoID(0)
-{
-	min = glm::vec3(0);
-	max = glm::vec3(0);
-}
 
-Shape::~Shape()
-{
-}
-
-/* copy the data from the shape to this object */
+// copy the data from the shape to this object
 void Shape::createShape(tinyobj::shape_t & shape)
 {
-		posBuf = shape.mesh.positions;
-		norBuf = shape.mesh.normals;
-		texBuf = shape.mesh.texcoords;
-		eleBuf = shape.mesh.indices;
+	posBuf = shape.mesh.positions;
+	norBuf = shape.mesh.normals;
+	texBuf = shape.mesh.texcoords;
+	eleBuf = shape.mesh.indices;
 }
 
-void Shape::measure() {
-  float minX, minY, minZ;
-   float maxX, maxY, maxZ;
+void Shape::measure()
+{
+	float minX, minY, minZ;
+	float maxX, maxY, maxZ;
 
-   minX = minY = minZ = 1.1754E+38F;
-   maxX = maxY = maxZ = -1.1754E+38F;
+	minX = minY = minZ = 1.1754E+38F;
+	maxX = maxY = maxZ = -1.1754E+38F;
 
-   //Go through all vertices to determine min and max of each dimension
-   for (size_t v = 0; v < posBuf.size() / 3; v++) {
-		if(posBuf[3*v+0] < minX) minX = posBuf[3*v+0];
-		if(posBuf[3*v+0] > maxX) maxX = posBuf[3*v+0];
+	//Go through all vertices to determine min and max of each dimension
+	for (size_t v = 0; v < posBuf.size() / 3; v++)
+	{
+		if (posBuf[3*v+0] < minX) minX = posBuf[3 * v + 0];
+		if (posBuf[3*v+0] > maxX) maxX = posBuf[3 * v + 0];
 
-		if(posBuf[3*v+1] < minY) minY = posBuf[3*v+1];
-		if(posBuf[3*v+1] > maxY) maxY = posBuf[3*v+1];
+		if (posBuf[3*v+1] < minY) minY = posBuf[3 * v + 1];
+		if (posBuf[3*v+1] > maxY) maxY = posBuf[3 * v + 1];
 
-		if(posBuf[3*v+2] < minZ) minZ = posBuf[3*v+2];
-		if(posBuf[3*v+2] > maxZ) maxZ = posBuf[3*v+2];
+		if (posBuf[3*v+2] < minZ) minZ = posBuf[3 * v + 2];
+		if (posBuf[3*v+2] > maxZ) maxZ = posBuf[3 * v + 2];
 	}
 
 	min.x = minX;
 	min.y = minY;
 	min.z = minZ;
-   max.x = maxX;
-   max.y = maxY;
-   max.z = maxZ;
+	max.x = maxX;
+	max.y = maxY;
+	max.z = maxZ;
 }
 
 void Shape::init()
 {
-   // Initialize the vertex array object
-   glGenVertexArrays(1, &vaoID);
-   glBindVertexArray(vaoID);
+	// Initialize the vertex array object
+	glGenVertexArrays(1, &vaoID);
+	glBindVertexArray(vaoID);
 
 	// Send the position array to the GPU
 	glGenBuffers(1, &posBufID);
@@ -70,18 +59,24 @@ void Shape::init()
 	glBufferData(GL_ARRAY_BUFFER, posBuf.size()*sizeof(float), &posBuf[0], GL_STATIC_DRAW);
 
 	// Send the normal array to the GPU
-	if(norBuf.empty()) {
+	if(norBuf.empty())
+	{
 		norBufID = 0;
-	} else {
+	}
+	else
+	{
 		glGenBuffers(1, &norBufID);
 		glBindBuffer(GL_ARRAY_BUFFER, norBufID);
 		glBufferData(GL_ARRAY_BUFFER, norBuf.size()*sizeof(float), &norBuf[0], GL_STATIC_DRAW);
 	}
 
 	// Send the texture array to the GPU
-	if(texBuf.empty()) {
+	if(texBuf.empty())
+	{
 		texBufID = 0;
-	} else {
+	}
+	else
+	{
 		glGenBuffers(1, &texBufID);
 		glBindBuffer(GL_ARRAY_BUFFER, texBufID);
 		glBufferData(GL_ARRAY_BUFFER, texBuf.size()*sizeof(float), &texBuf[0], GL_STATIC_DRAW);
@@ -104,7 +99,8 @@ void Shape::draw(const shared_ptr<Program> prog) const
 	int h_pos, h_nor, h_tex;
 	h_pos = h_nor = h_tex = -1;
 
-   glBindVertexArray(vaoID);
+	glBindVertexArray(vaoID);
+
 	// Bind position buffer
 	h_pos = prog->getAttribute("vertPos");
 	GLSL::enableVertexAttribArray(h_pos);
@@ -113,16 +109,20 @@ void Shape::draw(const shared_ptr<Program> prog) const
 
 	// Bind normal buffer
 	h_nor = prog->getAttribute("vertNor");
-	if(h_nor != -1 && norBufID != 0) {
+	if (h_nor != -1 && norBufID != 0)
+	{
 		GLSL::enableVertexAttribArray(h_nor);
 		glBindBuffer(GL_ARRAY_BUFFER, norBufID);
 		glVertexAttribPointer(h_nor, 3, GL_FLOAT, GL_FALSE, 0, (const void *)0);
 	}
 
-	if (texBufID != 0) {
+	if (texBufID != 0)
+	{
 		// Bind texcoords buffer
 		h_tex = prog->getAttribute("vertTex");
-		if(h_tex != -1 && texBufID != 0) {
+
+		if (h_tex != -1 && texBufID != 0)
+		{
 			GLSL::enableVertexAttribArray(h_tex);
 			glBindBuffer(GL_ARRAY_BUFFER, texBufID);
 			glVertexAttribPointer(h_tex, 2, GL_FLOAT, GL_FALSE, 0, (const void *)0);
@@ -136,10 +136,12 @@ void Shape::draw(const shared_ptr<Program> prog) const
 	glDrawElements(GL_TRIANGLES, (int)eleBuf.size(), GL_UNSIGNED_INT, (const void *)0);
 
 	// Disable and unbind
-	if(h_tex != -1) {
+	if (h_tex != -1)
+	{
 		GLSL::disableVertexAttribArray(h_tex);
 	}
-	if(h_nor != -1) {
+	if (h_nor != -1)
+	{
 		GLSL::disableVertexAttribArray(h_nor);
 	}
 	GLSL::disableVertexAttribArray(h_pos);
--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,24 +1,15 @@
+
 #include "Texture.h"
 #include "GLSL.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
 using namespace std;
 
-Texture::Texture() :
-	filename(""),
-	tid(0)
-{
-
-}
-
-Texture::~Texture()
-{
-
-}
 
 void Texture::init()
 {
@@ -26,13 +17,16 @@ void Texture::init()
 	int w, h, ncomps;
 	stbi_set_flip_vertically_on_load(true);
 	unsigned char *data = stbi_load(filename.c_str(), &w, &h, &ncomps, 0);
-	if(!data) {
+	if(! data)
+	{
 		cerr << filename << " not found" << endl;
 	}
-	if(ncomps != 3) {
+	if (ncomps != 3)
+	{
 		cerr << filename << " must have 3 components (RGB)" << endl;
 	}
-	if((w & (w - 1)) != 0 || (h & (h - 1)) != 0) {
+	if ((w & (w - 1)) != 0 || (h & (h - 1)) != 0)
+	{
 		cerr << filename << " must be a power of 2" << endl;
 	}
 	width = w;
@@ -42,17 +36,20 @@ void Texture::init()
 	glGenTextures(1, &tid);
 	// Bind the current texture to be the newly generated texture object
 	glBindTexture(GL_TEXTURE_2D, tid);
+
 	// Load the actual texture data
 	// Base level is 0, number of channels is 3, and border is 0.
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
 	// Generate image pyramid
 	glGenerateMipmap(GL_TEXTURE_2D);
+
 	// Set texture wrap modes for the S and T directions
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	// Set filtering mode for magnification and minimification
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+
 	// Unbind
 	glBindTexture(GL_TEXTURE_2D, 0);
 	// Free image, since the data is now on the GPU
